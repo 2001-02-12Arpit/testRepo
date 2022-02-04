d95883e7b9b1b8e56b6392791cd8060ea9e893db
arpit <arpitshukla62@gmail.com>
2022-02-04 20:13:31 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.serialization") version "1.4.21"
+}
+
+group = "me.arpit"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.31")
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
